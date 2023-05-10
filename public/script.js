307f093214830eeb7c6292c98d0538b0055465e8
Max <maxvanliempdt@gmail.com>
2023-05-10 11:13:57 +0200

    1.0.1

--- public/script.js
+++ public/script.js
@@ -12,8 +12,9 @@ document.querySelector("form").addEventListener("submit", (event) => {
 });
 
 socket.on("message", (message) => {
-  messages.insertAdjacentHTML("afterbegin", `<li>${message}</li>`);
-  messages.scrollTop = messages.scrollHeight;
+  const listItem = document.createElement("li");
+  listItem.textContent = `"${message}"`;
+  messages.insertBefore(listItem, messages.firstChild);
 });
 
 // Functie om quotes van de server te ontvangen en weer te geven
@@ -22,17 +23,44 @@ async function loadQuotes() {
     const response = await fetch("/quotes");
     const quotes = await response.json();
 
-    // Leegmaken van de <ul>
     messages.innerHTML = "";
 
-    // Toevoegen van de quotes aan de <ul> in omgekeerde volgorde
     for (let i = quotes.length - 1; i >= 0; i--) {
-      messages.insertAdjacentHTML("beforeend", `<li>${quotes[i].quote}</li>`);
+      messages.insertAdjacentHTML("beforeend", `<li>"${quotes[i].quote}"</li>`);
     }
   } catch (error) {
     console.error("Fout bij het laden van de quotes:", error);
   }
 }
+
+var url = 'https://api.api-ninjas.com/v1/quotes?category=';
+const getQuote = document.querySelector('form div p');
+
+function setQuoteInTextField() {
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': '1p/aMmyNR/hWCN736BiNug==0v5FiqrkcZCr06G7',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(function(result) {
+        var Randomquote = result[0].quote; // Verondersteld dat de quote zich in het eerste item van de resultaatarray bevindt
+        var textField = document.getElementById('message-input'); // Vervang 'myTextField' door de daadwerkelijke id van je tekstveld
+        textField.value = Randomquote;
+    })
+    .catch(function(error) {
+        console.error('Error: ', error);
+    });
+}
+
+getQuote.addEventListener("click", setQuoteInTextField);
 
 // Roep de functie aan wanneer de pagina wordt geladen
 window.addEventListener("load", loadQuotes);
